e4a7f82f7927736ff8f7cf6100340ef57e8ec4fe
joshiejack <joshjackwildman@gmail.com>
2021-06-20 21:56:15 +0100

    Siamese Fighting Fish texture. Fix Siamese Fighting Fish model.

--- src/main/java/uk/joshiejack/piscary/client/model/SiameseFightingFishModel.java
+++ src/main/java/uk/joshiejack/piscary/client/model/SiameseFightingFishModel.java
@@ -8,32 +8,34 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 public class SiameseFightingFishModel extends AbstractFishModel {
     public SiameseFightingFishModel() {
         texWidth = 32;
-        texHeight = 32;
+        texHeight = 16;
 
         tail = new ModelRenderer(this);
-        tail.setPos(0.0F, 23.0F, 3.0F);
-        tail.texOffs(0, 0).addBox(-1.0F, -1.0F, 4.0F, 2.0F, 2.0F, 3.0F, 0.0F, false);
-        tail.texOffs(0, 0).addBox(0.0F, -3.4F, 3.0F, 0.0F, 7.0F, 7.0F, 0.0F, false);
+        tail.setPos(0.0F, 23.0F, 0.5F);
+        setRotationAngle(tail, 0.0F, 0.0436F, 0.0F);
+        tail.texOffs(0, 0).addBox(-1.0F, -1.0F, 0.0F, 2.0F, 2.0F, 4.0F, 0.0F, false);
+        tail.texOffs(0, 0).addBox(0.0F, -3.4F, -0.5F, 0.0F, 7.0F, 7.0F, 0.0F, false);
 
         body = new ModelRenderer(this);
         body.setPos(0.0F, 24.0F, 0.0F);
-        body.texOffs(7, 2).addBox(-1.0F, -2.0F, 4.0F, 2.0F, 2.0F, 3.0F, 0.0F, false);
-        body.texOffs(0, 5).addBox(-1.0F, -1.6F, 3.0F, 2.0F, 1.0F, 1.0F, 0.0F, false);
+        body.texOffs(11, 11).addBox(-1.0F, -2.0F, -2.0F, 2.0F, 2.0F, 3.0F, 0.0F, false);
+        body.texOffs(8, 0).addBox(-1.0F, -1.6F, -3.0F, 2.0F, 1.0F, 1.0F, 0.0F, false);
 
         ModelRenderer fins = new ModelRenderer(this);
-        fins.setPos(-1.0F, 23.0F, -2.0F);
+        fins.setPos(-1.0F, -1.0F, -0.75F);
+        body.addChild(fins);
 
 
         ModelRenderer fin_right_r1 = new ModelRenderer(this);
-        fin_right_r1.setPos(0.0F, 0.0F, 0.0F);
+        fin_right_r1.setPos(0.0F, 0.0F, 1.0F);
         fins.addChild(fin_right_r1);
         setRotationAngle(fin_right_r1, -0.1745F, -0.6109F, 0.2618F);
-        fin_right_r1.texOffs(4, 12).addBox(0.0F, -0.5F, 0.0F, 0.0F, 1.0F, 2.0F, 0.0F, false);
+        fin_right_r1.texOffs(0, 1).addBox(0.0F, -0.5F, 0.0F, 0.0F, 1.0F, 2.0F, 0.0F, false);
 
         ModelRenderer fin_left_r1 = new ModelRenderer(this);
-        fin_left_r1.setPos(2.0F, 0.0F, 0.0F);
+        fin_left_r1.setPos(2.0F, 0.0F, 1.0F);
         fins.addChild(fin_left_r1);
         setRotationAngle(fin_left_r1, -0.1745F, 0.6109F, -0.2618F);
-        fin_left_r1.texOffs(0, 12).addBox(0.0F, -0.5F, 0.0F, 0.0F, 1.0F, 2.0F, 0.0F, false);
+        fin_left_r1.texOffs(0, 0).addBox(0.0F, -0.5F, 0.0F, 0.0F, 1.0F, 2.0F, 0.0F, false);
     }
 }
