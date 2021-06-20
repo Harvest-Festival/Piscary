67af23015946f4c1db6af0723ba0647eee35182d
joshiejack <joshjackwildman@gmail.com>
2021-06-20 20:00:19 +0100

    Manta Ray texture and model fix

--- src/main/java/uk/joshiejack/piscary/client/model/MantaRayModel.java
+++ src/main/java/uk/joshiejack/piscary/client/model/MantaRayModel.java
@@ -9,20 +9,19 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
 public class MantaRayModel extends RayModel {
-
 	public MantaRayModel() {
 		super(2F);
-		ModelRenderer filter1 = new ModelRenderer(this);
-		filter1.setPos(1.0F, 2.75F, -6.05F);
-		body.addChild(filter1);
-		setRotationAngle(filter1, -0.9599F, 0.0F, -0.1309F);
-		filter1.texOffs(0, 32).addBox(-4.0F, -2.0F, -8.0F, 2.0F, 7.0F, 2.0F, 0.0F, false);
+		ModelRenderer filter_1 = new ModelRenderer(this);
+		filter_1.setPos(1.0F, 2.75F, -6.05F);
+		body.addChild(filter_1);
+		setRotationAngle(filter_1, -0.9599F, 0.0F, -0.1309F);
+		filter_1.texOffs(0, 32).addBox(-4.0F, -2.0F, -6.0F, 2.0F, 7.0F, 2.0F, 0.0F, false);
 
-		ModelRenderer filter2 = new ModelRenderer(this);
-		filter2.setPos(1.0F, 2.75F, -6.05F);
-		body.addChild(filter2);
-		setRotationAngle(filter2, -0.9599F, 0.0F, 0.1309F);
-		filter2.texOffs(8, 32).addBox(0.0F, -2.0F, -8.0F, 2.0F, 7.0F, 2.0F, 0.0F, false);
+		ModelRenderer filter_2 = new ModelRenderer(this);
+		filter_2.setPos(1.0F, 2.75F, -6.05F);
+		body.addChild(filter_2);
+		setRotationAngle(filter_2, -0.9599F, 0.0F, 0.1309F);
+		filter_2.texOffs(8, 32).addBox(0.0F, -2.0F, -6.0F, 2.0F, 7.0F, 2.0F, 0.0F, false);
 	}
 
 	@Override
