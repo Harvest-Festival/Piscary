d2fc4d6342ba08c11171f4cbbc5a60620b1e2df2
joshiejack <joshjackwildman@gmail.com>
2021-06-20 21:20:17 +0100

    Piranha texture, fix fat fish model.

--- src/main/java/uk/joshiejack/piscary/client/model/FatFishModel.java
+++ src/main/java/uk/joshiejack/piscary/client/model/FatFishModel.java
@@ -8,34 +8,33 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 public class FatFishModel extends AbstractFishModel {
     public FatFishModel() {
         texWidth = 64;
-        texHeight = 64;
+        texHeight = 32;
 
         tail = new ModelRenderer(this);
-        tail.setPos(0.0F, 23.0F, 3.0F);
-        tail.texOffs(8, 6).addBox(0.0F, -5.8F, 14F, 0.0F, 2.0F, 2.0F, 0.0F, false);
-        tail.texOffs(8, 8).addBox(0.0F, -1.3F, 14F, 0.0F, 2.0F, 2.0F, 0.0F, false);
-        tail.texOffs(0, 4).addBox(0.0F, -4.3F, 10F, 0.0F, 4.0F, 4.0F, 0.0F, false);
+        tail.setPos(0.0F, 23.0F, 9.75F);
+        tail.texOffs(22, 2).addBox(0.0F, -6.3F, 4.0F, 0.0F, 8.0F, 4.0F, 0.0F, false);
+        tail.texOffs(0, 4).addBox(0.0F, -4.3F, 0.0F, 0.0F, 4.0F, 4.0F, 0.0F, false);
 
         body = new ModelRenderer(this);
         body.setPos(0.0F, 24.0F, 0.0F);
-        body.texOffs(0, 0).addBox(-2.0F, -8.0F, -1F, 4.0F, 8.0F, 14.0F, 0.0F, false);
-        body.texOffs(0, 0).addBox(-2.0F, -5.2F, -3F, 4.0F, 4.0F, 2.0F, 0.0F, false);
-        body.texOffs(8, 18).addBox(0.0F, -10.0F, 7F, 0.0F, 2.0F, 4.0F, 0.0F, false);
-        body.texOffs(0, 0).addBox(0.0F, 0.0F, 5F, 0.0F, 2.0F, 6.0F, 0.0F, false);
+        body.texOffs(0, 0).addBox(-2.0F, -8.0F, -3.0F, 4.0F, 8.0F, 14.0F, 0.0F, false);
+        body.texOffs(0, 0).addBox(-2.0F, -5.2F, -5.0F, 4.0F, 4.0F, 2.0F, 0.0F, false);
+        body.texOffs(0, 18).addBox(0.0F, -12.0F, 3.0F, 0.0F, 4.0F, 6.0F, 0.0F, false);
+        body.texOffs(0, 0).addBox(0.0F, 0.0F, 3.0F, 0.0F, 2.0F, 6.0F, 0.0F, false);
 
         ModelRenderer fins = new ModelRenderer(this);
         fins.setPos(-1.0F, 23.0F, -2.0F);
 
         ModelRenderer fin_right_r1 = new ModelRenderer(this);
-        fin_right_r1.setPos(-1.0F, -1.0F, -2.0F);
+        fin_right_r1.setPos(-1.0F, -1.0F, 3.0F);
         fins.addChild(fin_right_r1);
         setRotationAngle(fin_right_r1, -0.1745F, -0.6109F, 0.2618F);
-        fin_right_r1.texOffs(0, 18).addBox(0.0F, -1.0F, 7F, 0.0F, 2.0F, 4.0F, 0.0F, false);
+        fin_right_r1.texOffs(0, 18).addBox(0.0F, -1.0F, 0.0F, 0.0F, 2.0F, 4.0F, 0.0F, false);
 
         ModelRenderer fin_left_r1 = new ModelRenderer(this);
-        fin_left_r1.setPos(3.0F, -1.0F, -2.0F);
+        fin_left_r1.setPos(3.0F, -1.0F, 3.0F);
         fins.addChild(fin_left_r1);
         setRotationAngle(fin_left_r1, -0.1745F, 0.6109F, -0.2618F);
-        fin_left_r1.texOffs(0, 8).addBox(0.0F, -1.0F, 7F, 0.0F, 2.0F, 4.0F, 0.0F, false);
+        fin_left_r1.texOffs(0, 8).addBox(0.0F, -1.0F, 0.0F, 0.0F, 2.0F, 4.0F, 0.0F, false);
     }
 }
